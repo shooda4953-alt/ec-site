adf06e9359178879731cfd33d35fc0190e1d9d84
sho_oda <sho_oda@overtech.tokyo>
2024-01-19 17:21:43 +0900

    フォームログイン処理修正

--- src/components/molecules/LoginForm.tsx
+++ src/components/molecules/LoginForm.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import EmailInput from "../atoms/EmailInput";
 import PasswordInput from "../atoms/PasswordInput";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { useNavigate } from "react-router-dom";
 
 const LoginForm: React.FC = () => {
@@ -10,67 +10,70 @@ const LoginForm: React.FC = () => {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [csrfToken, setCsrfToken] = useState("");
+  const [loginError, setLoginError] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     // ログイン画面を表示する際にCSRFトークンを取得
-    async function fetchCsrfToken() {
+    const fetchCsrfToken = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5182/login/csrf/token"
+          "http://localhost:5173/login/csrf/token"
         );
+        console.log(response.data);
         setCsrfToken(response.data.csrfToken);
       } catch (error) {
         console.error("Failed to fetch CSRF token:", error);
       }
-    }
+    };
 
     fetchCsrfToken();
   }, []);
 
-  const handleEmailChange = (value: string) => {
-    setEmail(value);
-    setEmailError("");
-  };
-
-  const handlePasswordChange = (value: string) => {
-    setPassword(value);
-    setPasswordError("");
-  };
-
-  // APIのベースURL
-  const baseURL = "http://127.0.0.1:3658/m1/440876-0-default";
-
-  // エンドポイントのパス
-  const endpointPath = "/auth/signin";
-
-  // リクエストデータ
-  const requestData = {
-    email: email,
-    password: password,
+  const handleInputChange = (
+    value: string,
+    setter: React.Dispatch<React.SetStateAction<string>>,
+    errorSetter: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    setter(value);
+    errorSetter("");
+    setLoginError(""); // 入力が変更されたらエラーメッセージをクリア
   };
 
   const handleLogin = async () => {
     // 入力の検証
     if (!email) {
       setEmailError("メールアドレスは必須項目です");
+      return;
     }
 
     if (!password) {
       setPasswordError("パスワードは必須項目です");
+      return;
     }
-
-    console.log("clickButton");
 
     // ログインの処理を実装
     if (email && password) {
-      // ログインリクエストの送信
+      const data = JSON.stringify({
+        email: "user@lh.sandbox",
+        password: "pass",
+      });
+
       try {
-        await axios.post(`${baseURL}${endpointPath}`, requestData, {
+        const config: AxiosRequestConfig = {
+          method: "post",
+          url: "http://localhost:8080/auth/signin",
           headers: {
+            // "User-Agent": "Apidog/1.0.0 (https://apidog.com)",
+            // "Content-Type": "application/json",
             "X-CSRF-Token": csrfToken,
           },
-        });
+          data: data,
+        };
+
+        const response = await axios(config);
+
+        console.log(JSON.stringify(response.data));
 
         // パスが "/admin/login" の場合は "/admin" に遷移
         const currentPath = window.location.pathname;
@@ -80,44 +83,41 @@ const LoginForm: React.FC = () => {
           navigate("/");
         }
       } catch (error) {
-        console.error("Login failed:", error);
-        // エラー処理をここに追加
+        console.error("Login error:", error);
+        setLoginError("メールアドレスまたはパスワードが誤っています");
       }
     }
   };
 
   return (
     <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      <div
-        style={{
-          marginBottom: "20px",
-        }}
-      >
-        <EmailInput label="Email" value={email} onChange={handleEmailChange} />
+      <div style={{ marginBottom: "20px" }}>
+        <EmailInput
+          label="Email"
+          value={email}
+          onChange={(value) =>
+            handleInputChange(value, setEmail, setEmailError)
+          }
+        />
         {emailError && <div style={{ color: "red" }}>{emailError}</div>}
       </div>
       <div style={{ marginBottom: "20px" }}>
         <PasswordInput
           label="Password"
           value={password}
-          onChange={handlePasswordChange}
+          onChange={(value) =>
+            handleInputChange(value, setPassword, setPasswordError)
+          }
         />
         {passwordError && <div style={{ color: "red" }}>{passwordError}</div>}
       </div>
       <div
-        style={{
-          marginTop: "20px",
-          textAlign: "center",
-          alignItems: "center",
-        }}
+        style={{ marginTop: "20px", textAlign: "center", alignItems: "center" }}
       >
         <button onClick={handleLogin}>Login</button>
+        {loginError && <div style={{ color: "red" }}>{loginError}</div>}
       </div>
     </div>
   );
